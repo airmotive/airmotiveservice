f2a7b60ed9f1070b776c0f89233389ecfab10a20
netuser_py <netuser-biolyrics@usa.net>
2020-02-24 13:29:28 -0700

    validate

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,6 @@
 function validateForm() {
-    var x = document.forms["myForm"]["answer"].value.toLowerCase();										
+    var x = document.forms["myForm"]["answer"].value.toLowerCase();	
+    alert("validate...");									
     if (x !== "sky") {
         alert("Try Again! Where do we fly?");
         return false;
