eba0cf813eaa22c9c6c8e83ba3b40ba594770f66
Airmotive <46230214+airmotive@users.noreply.github.com>
2020-02-23 23:38:14 -0700

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,7 @@
+function validateForm() {
+    var x = document.forms["myForm"]["answer"].value.toLowerCase();										
+    if (x !== "sky") {
+        alert("Try Again! Where do we fly?");
+        return false;
+    }
+}
