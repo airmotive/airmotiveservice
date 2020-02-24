c76d41ba2d19f9611dfd6dbdc7f4a6b038b26d34
Airmotive <46230214+airmotive@users.noreply.github.com>
2020-02-23 23:39:28 -0700

    Delete myscript.js

--- myscript.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    var x = document.forms["myForm"]["answer"].value.toLowerCase();										
-    if (x !== "sky") {
-        alert("Try Again! Where do we fly?");
-        return false;
-    }
-}
