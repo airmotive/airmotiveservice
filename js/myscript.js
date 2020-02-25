056b69c2e2850015644efd7ddb7dcc7622ac2514
Airmotive <46230214+airmotive@users.noreply.github.com>
2020-02-25 16:46:25 -0700

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,5 @@
 function validateForm() {
-    var x = document.forms["myForm"]["answer"].value.toLowerCase();				
+    var x = document.forms["myForm"]["answer"].value.toLowerCase().trim();				
     if (x !== "sky") {
         alert("Try Again! Where do we fly?");
         return false;
