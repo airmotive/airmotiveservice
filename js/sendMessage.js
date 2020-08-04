27f17c019b074283ab192e7fd723959550972aca
netuser_py <netuser-biolyrics@usa.net>
2020-08-04 00:51:54 -0600

    new send form

---
+++ js/sendMessage.js
@@ -0,0 +1,17 @@
+// http://jsfiddle.net/228d4snb/
+var message = "";
+
+$("#sendMessage").on("click", function() { 
+    message = $("#contactform").serialize(); 
+    // alert(message);
+    $.ajax({
+        url: "https://formspree.io/mbjzwzpv", 
+        method: "POST",
+        data: {message: message},
+        dataType: "json"
+    });
+    
+    alert('Thanks for the email, we\'ll be in touch soon.');
+    window.open("https://airmotiveservice.com/", "_self");
+    return false;
+});	
